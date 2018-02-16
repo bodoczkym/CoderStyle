de75c0352b8c4eb420a7839f770b81e98c5f3d7e
Mike Bodoczky <shaddox27@yahoo.com>
2018-02-16 20:03:39 +0000

    Lesson-2

--- app/src/main/java/com/developer/shaddox/coderstyle/Model/Category.kt
+++ app/src/main/java/com/developer/shaddox/coderstyle/Model/Category.kt
@@ -3,4 +3,8 @@ package com.developer.shaddox.coderstyle.Model
 /**
  * Created by Shaddox on 2018. 02. 16..
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
